51c1311463f95508c4e0fc421e7847fe17b71d61
r0man0v-r0man <45589927+r0man0v-r0man@users.noreply.github.com>
2019-12-07 10:41:03 +0300

    add exceptions and update anylizers

---
+++ Domain/Exceptions/FlatNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class FlatNotFoundException : Exception
+    {
+        public FlatNotFoundException(string message, Exception ex)
+            :base(message, ex)
+        {
+
+        }
+
+        public FlatNotFoundException()
+        {
+        }
+
+        public FlatNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}

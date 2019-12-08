0678d4d1867d3ccdc4d49f46ffaf9ff34322f55e
r0man0v-r0man <45589927+r0man0v-r0man@users.noreply.github.com>
2019-12-08 15:59:46 +0300

    add unitOfWork

---
+++ Infrastructure/Persistance/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Application;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly Advertiser2DbContext _context;
+        private bool _disposed = false;
+
+        public async Task<int> Complite()
+        {
+            int result = await _context.SaveChangesAsync().ConfigureAwait(false);
+            return result;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+    }
+}
